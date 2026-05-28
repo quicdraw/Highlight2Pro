dbb01dd669b337636c528463daec324ba0027936
quicdraw <degrawea@gmail.com>
2026-05-27 22:32:01 -0400

    Building initial structure

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import src.extractor
+
+def main():
+    print("Welcome to Highlights2Pro")
+    print("Please provide the name of the document containing the needed highlights:")
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
